998d0e5325f7a601d4e39823621477c58f93a516
Adel Bassiony <a.bassiony@icloud.com>
2020-09-21 03:35:01 +0200

    Fixes & Improvements on App file

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import PersonalDetails from './components/PersonalDetails/PersonalDetails';
+import WorkDetails from './components/WorkDetails/WorkDetails';
+import Avatar from './components/Avatar/Avatar';
+import EmailVerification from './components/EmailVerification/EmailVerification';
+import Success from './components/Success/Success';
+import ProgressBar from './components/ProgressBar/ProgressBar';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Provider store={store}>
+            <ProgressBar />
+            <NavigationContainer>  
+                <Stack.Navigator initialRouteName="PersonalDetails">
+                <Stack.Screen name="PersonalDetails" component={PersonalDetails} options={{ headerShown: false }} />
+                <Stack.Screen name="WorkDetails" component={WorkDetails} options={{ headerShown: false }} />
+                <Stack.Screen name="Avatar" component={Avatar} options={{ headerShown: false }} />
+                <Stack.Screen name="EmailVerification" component={EmailVerification} options={{ headerShown: false }} />
+                <Stack.Screen name="Success" component={Success} options={{ headerShown: false }} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
